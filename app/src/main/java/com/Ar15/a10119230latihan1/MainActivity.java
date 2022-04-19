3dbcc1ab2a54c8afd858d2b569bd6bb25ccb4b39
AkmalR15 <csxenomorp@gmail.com>
2022-04-19 14:11:47 +0700

    19/04/2022

---
+++ app/src/main/java/com/Ar15/a10119230latihan1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.Ar15.a10119230latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Nama                  : Akmal Rizqulloh
+//NIM                   : 10119230
+//Kelas                 : IF-6
+//Tanggal Pengerjaan    : 17-04-2022
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
